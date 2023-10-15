1074b3a2141c496c5545174bb5b5887a314f3b74
Ahmed Saad <ahmedsaad@Ahmeds-Laptop.cable.rcn.com>
2023-10-14 20:16:16 -0400

    updates

--- js/memberList.js
+++ js/memberList.js
@@ -1,47 +1,42 @@
 const board = [
   {
-    name: "Abdulhaseeb Mohammed",
+    name:"Musa/Haseeb",
+    position: "Presidents of the president",
+    desc: "",
+    male: true,
+  },
+  {
+    name:"Sulaiman",
     position: "President",
     desc: "I will try my best to stick to Quran, sunnah and what the scholars say to run the msa. Allah swt tells us in the Quran in surah shuara ayah 13 to establish the deen and I intend to do that with the help of allah and everyone at the msa who are upon haqq.",
     male: true,
   },
   {
-    name: "Taskin Zaman",
+    name: "Taskin",
     position: "Vice President",
     desc: "When I look at the sky, I tend to get lost. The sky reminds me of the ayah from Surah Mulk when Allah says there are no flaws in the sky and no matter how many times we look, we will not find any. Only our eyes will become frustrated and weary. {Surah Mulk, 3-4}",
     male: false,
   },
   {
-    name: "Ahmad Saad",
-    position: "Trasurer",
-    desc: "Inshallah I want to follow the sunnah of the prophet saw and spread the deen of allah. Sura nisa ayah 59 says “Should you disagree on anything, then refer it to Allah and His Messenger” and inshallah I will base all my decisions on what allah and his rasul said",
-    male: true,
-  },
-  {
-    name: "Swera",
-    position: "Secretary",
-    desc: "Bridge to Allah: We were created to be tested, I feel Allah tailors these tests to connect you to Him. I’m grateful for the tests Allah has given me that have been my way of strengthening my connection with Allah, Al-Wadud.",
+    name: "Waleed",
+    position: "Treasurer",
+    desc: "When I look at the sky, I tend to get lost. The sky reminds me of the ayah from Surah Mulk when Allah says there are no flaws in the sky and no matter how many times we look, we will not find any. Only our eyes will become frustrated and weary. {Surah Mulk, 3-4}",
     male: false,
   },
+
 ];
 
 const tech = [
   {
     name: "Ahnaf Shamim",
     position: "Tech Manager",
-    desc: "Married to Ahmed Saad",
-    male: true,
-  },
-  {
-    name: "Ahmad Saad",
-    position: "Co-Tech Manager",
-    desc: "Married To Munir",
+    desc: "",
     male: true,
   },
   {
     name: "Muhammad Munir",
     position: "Developer",
-    desc: "Married to Ahnaf Shamim and Ahmad",
+    desc: "Married to Ahnaf Shamin",
     male: true,
   },
   {
